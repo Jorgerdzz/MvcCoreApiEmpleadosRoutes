794686abfb60b34ca2eabfe2c05d527b76cf9bde
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-03-25 14:07:03 +0100

    Add project files.

---
+++ MvcCoreApiEmpleadosRoutes/Controllers/EmpleadosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreApiEmpleadosRoutes.Services;
+using NugetApiModels.Models;
+using System.Threading.Tasks;
+
+namespace MvcCoreApiEmpleadosRoutes.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ServiceEmpleados service;
+
+        public EmpleadosController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index(string? oficio)
+        {
+            List<string> oficios = await this.service.GetOficiosAsync();
+            List<Empleado> empleados = await this.service.GetEmpleadosAsync();
+            ViewData["OFICIOS"] = oficios;
+            if(oficio != null)
+            {
+                List<Empleado> empleadosOficio = await this.service.GetEmpleadosOficiosAsync(oficio);
+                return View(empleadosOficio);
+            }
+            return View(empleados);
+        }
+    }
+}
---
+++ MvcCoreApiEmpleadosRoutes/Services/ServiceEmpleados.cs
@@ -0,0 +1,85 @@
+﻿using NugetApiModels.Models;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MvcCoreApiEmpleadosRoutes.Services
+{
+    public class ServiceEmpleados
+    {
+        private string apiUrl;
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceEmpleados(IConfiguration configuration)
+        {
+            this.apiUrl = configuration.GetValue<string>("ApiUrls:ApiEmpleados");
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string request = "api/empleados";
+                client.BaseAddress = new Uri(this.apiUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Empleado> data = await response.Content.ReadAsAsync<List<Empleado>>();
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public async Task<List<string>> GetOficiosAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/empleados/oficios";
+                client.BaseAddress = new Uri(this.apiUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<string> data = await response.Content.ReadAsAsync<List<string>>();
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosOficiosAsync(string oficio)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/Empleados/EmpleadosByOficio/" + oficio;
+                client.BaseAddress = new Uri(this.apiUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Empleado> data = await response.Content.ReadAsAsync<List<Empleado>>();
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+    }
+}
